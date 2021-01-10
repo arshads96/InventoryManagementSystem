257b67f2f96d06c347826315a7569f6439dbfd2a
shubhampal261 <shubhampal261@gmail.com>
2021-01-10 16:01:01 +0530

    initial commit

--- src/main/java/com/arshad/monolith/InventoryManagementSystem/controller/CustomerController.java
+++ src/main/java/com/arshad/monolith/InventoryManagementSystem/controller/CustomerController.java
@@ -24,35 +24,34 @@ public class CustomerController {
     private CustomerService customerServiceImpl;
 
     @GetMapping
-    public List<CustomerResponseModel> getAllCustomers(){
+    public List<CustomerResponseModel> getAllCustomers() {
         return customerServiceImpl.getAllCustomers();
     }
 
     @GetMapping(path = "/{id}")
-    public CustomerResponseModel getCustomer(@PathVariable int id){
+    public CustomerResponseModel getCustomer(@PathVariable int id) {
         CustomerResponseModel customer = customerServiceImpl.getCustomerByID(id);
-        if(customer == null){
-            throw new CustomerNotFoundException(String.format(CustomerConstants.CANNOT_FIND_Customer,id));
+        if (customer == null) {
+            throw new CustomerNotFoundException(String.format(CustomerConstants.CANNOT_FIND_Customer, id));
         }
         return customer;
     }
 
     @PostMapping
-    public ResponseEntity addCustomer(@RequestBody Customer customer){
+    public ResponseEntity addCustomer(@RequestBody Customer customer) {
         customerServiceImpl.addCustomer(customer);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(customer.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity deleteCustomer(@PathVariable int id){
+    public ResponseEntity deleteCustomer(@PathVariable int id) {
         CustomerResponseModel customer = customerServiceImpl.deleteCustomerById(id);
-        if(customer == null){
-            throw new CustomerNotFoundException(String.format(CustomerConstants.CANNOT_DELETE_Customer,id));
+        if (customer == null) {
+            throw new CustomerNotFoundException(String.format(CustomerConstants.CANNOT_DELETE_Customer, id));
         }
         return ResponseEntity.noContent().build();
     }
-
 
 
 }
